c94a756973edde7f899da8db39e2f88bac5f542b
David Bonnes <david@bonnes.me>
2019-02-07 22:08:24 +0000

    typo

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
 	name = 'intouchclient',
 	version = '0.1.0',
-	description = 'Python client for connecting to Lan2RF gateway',
-	url = 'https://github.com/zxdavb/evohome-client/',
+	description = 'Python client for connecting devices via a Lan2RF gateway',
+	url = 'https://github.com/zxdavb/intouch-client/',
 	download_url = 'https://github.com/zxdavb/intouch-client/tarball/0.2.8',
 	author = 'David Bonnes',
-	author_email = 'evohome@andrew-stock.com',
+	author_email = 'intouch@xxx.com',
 	license = 'Apache 2',
 	classifiers = [
 		'Development Status :: 3 - Alpha',
