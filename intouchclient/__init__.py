932799a49cbbe288703b04680f4d8cb62aa0fc74
David Bonnes <david@bonnes.me>
2019-02-07 22:46:25 +0000

    woops

--- intouchclient/__init__.py
+++ intouchclient/__init__.py
@@ -76,7 +76,7 @@ class Gateway(InComfortClient):
             # self.async_status()
         # )
 
-        await self.async_status()
+        await self.async_status() # await
 
 #       loop.close()
 
