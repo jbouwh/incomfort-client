a9104bca647f509d25c9928713fd27451e55b471
David Bonnes <david@bonnes.me>
2019-05-02 09:58:05 +0100

    switch to incomfort

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-VERSION = "0.2.4"
+VERSION = "0.2.5"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="intouch-client",
+    name="incomfort-client",
     version=VERSION,
     author="David Bonnes",
     author_email="zxdavb@gmail.com",
-    description="A aiohttp-based client for Intergas Intouch Lan2RF systems",
+    description="A aiohttp-based client for Intergas InComfort/InTouch Lan2RF systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zxdavb/intouch-client",
-    download_url = 'https://github.com/zxdavb/intouch-client/archive/VERSION.tar.gz',
+    url="https://github.com/zxdavb/incomfort-client",
+    download_url = 'https://github.com/zxdavb/incomfort-client/archive/VERSION.tar.gz',
     packages=setuptools.find_packages(),
-    keywords = ['intergas', 'intouch', 'lan2rf'],
+    keywords=['intergas', 'incomfort', 'intouch', 'lan2rf'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
