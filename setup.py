103794eb6ae193baf74752514b1a313d863a53aa
David Bonnes <david@bonnes.me>
2021-03-10 16:51:19 +0000

    0.4.5 - tweak setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,54 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup.py file."""
 
-VERSION = "0.4.4"
+import os
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+import sys
+
+VERSION = "0.4.5"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version."""
+
+    description = "verify that the git tag matches our version"
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
+setup(
     name="incomfort-client",
     version=VERSION,
     author="David Bonnes",
-    author_email="zxdavb@gmail.com",
-    description="A aiohttp-based client for Intergas InComfort/InTouch Lan2RF systems",
+    author_email="zxdavb@bonnes.me",
+    description="An aiohttp-based client for Intergas InComfort/InTouch Lan2RF systems",
+    keywords=["intergas", "incomfort", "intouch", "lan2rf"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zxdavb/incomfort-client",
     download_url="https://github.com/zxdavb/incomfort-client/archive/VERSION.tar.gz",
-    packages=setuptools.find_packages(),
-    keywords=["intergas", "incomfort", "intouch", "lan2rf"],
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["aiohttp"],
+    cmdclass={
+        "verify": VerifyVersionCommand,
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
     ],
 )
