9f5e27df3ecd17fd3ece24b603e9f0986900bf18
Joostlek <joostlek@outlook.com>
2024-06-08 12:53:14 +0200

    Migrate to pyproject.toml

--- tests/common.py
+++ tests/common.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 import aiohttp
 from aioresponses import aioresponses
 
-from incomfortclient import HEATERLIST, Gateway, Heater
+from src.incomfortclient import HEATERLIST, Gateway, Heater
 
 HOSTNAME = "192.168.0.1"
 
